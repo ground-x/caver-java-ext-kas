915bc4a58703a5a110f91b29ee342d73a41c15db
kale.kim <kale.kim@groundx.xyz>
2020-08-13 16:49:43 +0900

    add AnchorAPIV1 class

--- core/src/main/java/com/klaytn/caver/kas/anchor/v1/AnchorAPIV1.java
+++ core/src/main/java/com/klaytn/caver/kas/anchor/v1/AnchorAPIV1.java
@@ -2,27 +2,93 @@ package com.klaytn.caver.kas.anchor.v1;
 
 import anchor.v1.ApiClient;
 import anchor.v1.ApiException;
+import anchor.v1.ApiResponse;
 import anchor.v1.api.AnchorApi;
-import anchor.v1.model.Payload;
-import anchor.v1.model.V1AnchorRequest;
+import anchor.v1.model.*;
 
 import java.util.Map;
 
 public class AnchorAPIV1 {
+    AnchorApi anchorApi;
+    String url;
+    String authorization;
+    String chainId;
 
-    public static void sendAnchoringData(String operatorId, Map payload, AccessOptions accessOptions) throws ApiException {
-        AnchorApi api = new AnchorApi();
-        api.getApiClient().setDebugging(true);
+    public AnchorAPIV1(String url, String authorization, String chainId) {
+        this.url = url;
+        this.authorization = authorization;
+        this.chainId = chainId;
 
-        V1AnchorRequest body = new V1AnchorRequest();
-        body.setOperator(operatorId);
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(url);
+        this.anchorApi = new AnchorApi(apiClient);
+    }
 
-        Payload payload1 = new Payload();
-        payload1.setBlockNumber(5);
-        payload1.setCustomField("custom");
+    public V1AnchorResponse sendAnchoringData(String operatorId, Map payload) throws ApiException {
+        V1AnchorRequest anchorRequest = new V1AnchorRequest();
+        anchorRequest.setOperator(operatorId);
+        anchorRequest.setPayload(payload);
 
-        body.setPayload(payload1);
+        return getAnchorApi().anchorBlock(getAuthorization(), "", anchorRequest);
+    }
 
-        api.anchorBlock(accessOptions.getAuthorizationString(), accessOptions.getKrn().get(0), body);
+    public V1OperatorTxResponse getAnchoringTransaction(String operatorId, AnchorQueryParams queryParams) throws ApiException {
+        if(queryParams != null) {
+            return anchorApi.retrieveBlock(operatorId, getAuthorization(), null, queryParams.getSize(), queryParams.getFromDate(), queryParams.toDate, queryParams.getCursor());
+        } else {
+            return anchorApi.retrieveBlock(operatorId, getAuthorization(), null, 0, 0, 0, null);
+        }
+    }
+
+    public V1OperatorTxResponse1 getAnchoringTransactionByTxHash(String operatorId, String txHash) throws ApiException {
+        return anchorApi.retrieveanchoredtransaction(operatorId, txHash, getAuthorization(), null);
+    }
+
+    public V1OperatorPayloadResponse getAnchoringTransactionByPayloadId(String operatorId, String payloadId) throws ApiException {
+        return anchorApi.getRetrieveanchoredtransaction(operatorId, payloadId, getAuthorization(), null);
+    }
+
+    public V1OperatorResponse getOperators(AnchorQueryParams queryParams) throws ApiException {
+        if(queryParams != null) {
+            return anchorApi.retrieveregisteredservicechainoperators(getAuthorization(), null, queryParams.getSize(), queryParams.getFromDate(), queryParams.toDate, queryParams.getCursor());
+        } else {
+            return anchorApi.retrieveregisteredservicechainoperators(getAuthorization(), null, 0, 0, 0, null);
+        }
+    }
+
+    public V1OperatorResponse1 getOperator(String operatorId) throws ApiException {
+        return anchorApi.retrieveOperator(getAuthorization(), null, operatorId);
+    }
+
+    public AnchorApi getAnchorApi() {
+        return anchorApi;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getAuthorization() {
+        return authorization;
+    }
+
+    public String getChainId() {
+        return chainId;
+    }
+
+    public void setAnchorApi(AnchorApi anchorApi) {
+        this.anchorApi = anchorApi;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
+    }
+
+    public void setChainId(String chainId) {
+        this.chainId = chainId;
     }
 }
---
+++ core/src/main/java/com/klaytn/caver/kas/anchor/v1/AnchorQueryParams.java
@@ -0,0 +1,50 @@
+package com.klaytn.caver.kas.anchor.v1;
+
+public class AnchorQueryParams {
+    int size;
+    int fromDate;
+    int toDate;
+    String cursor;
+
+    public AnchorQueryParams() {
+    }
+
+    public AnchorQueryParams(int size, int formDate, int toDate, String cursor) {
+        this.size = size;
+        this.fromDate = formDate;
+        this.toDate = toDate;
+        this.cursor = cursor;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getFromDate() {
+        return fromDate;
+    }
+
+    public int getToDate() {
+        return toDate;
+    }
+
+    public String getCursor() {
+        return cursor;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setFromDate(int fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public void setToDate(int toDate) {
+        this.toDate = toDate;
+    }
+
+    public void setCursor(String cursor) {
+        this.cursor = cursor;
+    }
+}
