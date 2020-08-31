94927faebb9cead08b214f1420d7577f4614209b
kale.kim <kale.kim@groundx.xyz>
2020-08-31 14:05:01 +0900

    add description.

--- core/src/main/java/com/klaytn/caver/ext/kas/anchor/AnchorQueryOptions.java
+++ core/src/main/java/com/klaytn/caver/ext/kas/anchor/AnchorQueryOptions.java
@@ -2,15 +2,44 @@ package com.klaytn.caver.ext.kas.anchor;
 
 import com.klaytn.caver.ext.kas.utils.KASUtils;
 
+/**
+ * Representing a query parameters where using Anchor REST API.
+ */
 public class AnchorQueryOptions {
+
+    /**
+     * Maximum number of data to query.
+     */
     long size;
+
+    /**
+     * The starting date of the data to be queried.
+     */
     String fromDate;
+
+    /**
+     * The ending date of the data to be queried.
+     */
     String toDate;
+
+    /**
+     * Information of the last retrieved cursor.
+     */
     String cursor;
 
+    /**
+     * Creates an AnchorQueryOptions instance.
+     */
     public AnchorQueryOptions() {
     }
 
+    /**
+     * Creates an AnchorQueryOptions instance.
+     * @param size Maximum number of data to query.
+     * @param fromDate The starting date of the data to be queried.
+     * @param toDate The ending date of the data to be queried.
+     * @param cursor Information of the last retrieved cursor.
+     */
     public AnchorQueryOptions(long size, String fromDate, String toDate, String cursor) {
         this.size = size;
         setFromDate(fromDate);
@@ -18,42 +47,66 @@ public class AnchorQueryOptions {
         this.cursor = cursor;
     }
 
+    /**
+     * Getter function for size.
+     * @return long
+     */
     public long getSize() {
         return size;
     }
 
+    /**
+     * Setter function for size.
+     * @param size Maximum number of data to query.
+     */
     public void setSize(long size) {
         this.size = size;
     }
 
+    /**
+     * Getter function for fromDate.
+     * @return String
+     */
     public String getFromDate() {
         return fromDate;
     }
 
+    /**
+     * Setter function for fromDate
+     * @param fromDate The starting date of the data to be queried.
+     */
     public void setFromDate(String fromDate) {
         this.fromDate = KASUtils.convertDateToTimestamp(fromDate);
     }
 
-    public void setFromDate(long fromDate) {
-        this.fromDate = Long.toString(fromDate);
-    }
-
+    /**
+     * Getter function for toDate
+     * @return String
+     */
     public String getToDate() {
         return toDate;
     }
 
+    /**
+     * Setter function for toDate
+     * @param toDate
+     */
     public void setToDate(String toDate) {
         this.toDate = KASUtils.convertDateToTimestamp(toDate);
     }
 
-    public void setToDate(long toDate) {
-        this.toDate = Long.toString(toDate);
-    }
-
+    /**
+     * Getter function for cursor
+     * @return String
+     */
     public String getCursor() {
         return cursor;
     }
 
+    /**
+     * Setter function for cursor
+     * @param cursor Information of the last retrieved cursor.
+     */
     public void setCursor(String cursor) {
         this.cursor = cursor;
     }
