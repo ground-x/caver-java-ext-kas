66f31748ce40bb3fc8f2bf8b77e4fcc7447c77d5
kale.kim <kale.kim@groundx.xyz>
2020-09-17 18:19:28 +0900

    add KeyTypeFail class

--- core/src/main/java/com/klaytn/caver/ext/kas/wallet/accountkey/KeyTypeFail.java
+++ core/src/main/java/com/klaytn/caver/ext/kas/wallet/accountkey/KeyTypeFail.java
@@ -1,2 +1,11 @@
-package com.klaytn.caver.ext.kas.wallet.accountKeyType;public class AccountKeyFail {
+package com.klaytn.caver.ext.kas.wallet.accountkey;
+
+import io.swagger.client.api.wallet.model.EmptyUpdateKeyType;
+
+public class KeyTypeFail extends EmptyUpdateKeyType {
+    public static final long KEY_TYPE = 3;
+
+    public KeyTypeFail() {
+        super.setKeyType(KEY_TYPE);
+    }
 }
