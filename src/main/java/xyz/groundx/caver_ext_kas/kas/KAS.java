78fd6cfc0d87c90350b7e71b3c369c1279313695
kale.kim <kale.kim@groundx.xyz>
2020-10-06 11:36:07 +0900

    add description.

--- src/main/java/xyz/groundx/caver_ext_kas/kas/KAS.java
+++ src/main/java/xyz/groundx/caver_ext_kas/kas/KAS.java
@@ -21,14 +21,38 @@ import xyz.groundx.caver_ext_kas.kas.tokenhistory.TokenHistoryAPI;
 import xyz.groundx.caver_ext_kas.kas.wallet.WalletAPI;
 import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.ApiClient;
 
+/**
+ * Representing a wrapping class to use KAS API in rest_client package.
+ */
 public class KAS {
+    /**
+     * The Anchor API instance.
+     */
     private AnchorAPI anchor;
+
+    /**
+     * The Token history API instance.
+     */
     private TokenHistoryAPI tokenHistory;
+
+    /**
+     * The Wallet API instance.
+     */
     private WalletAPI wallet;
 
+    /**
+     * Creates a KAS instance.
+     */
     public KAS() {
     }
 
+    /**
+     * Initialize Anchor API.
+     * @param url An URL to request Anchor API.
+     * @param chainId The Klaytn network chain id.
+     * @param accessKeyId The access key provided by KAS console.
+     * @param secretAccessKey The secret key provided by KAS console.
+     */
     public KAS initAnchorAPI(String url, String chainId, String accessKeyId, String secretAccessKey) {
         ApiClient apiClient = new ApiClient();
         apiClient.setBasePath(url);
@@ -39,6 +63,13 @@ public class KAS {
         return this;
     }
 
+    /**
+     * Initialize Wallet API.
+     * @param url An URL to request Wallet API.
+     * @param chainId The Klaytn network chain id.
+     * @param accessKeyId The access key provided by KAS console.
+     * @param secretAccessKey The secret key provided by KAS console.
+     */
     public KAS initWalletAPI(String url, String chainId, String accessKeyId, String secretAccessKey) {
         ApiClient apiClient = new ApiClient();
         apiClient.setBasePath(url);
@@ -49,6 +80,13 @@ public class KAS {
         return this;
     }
 
+    /**
+     * Initialize Token History API.
+     * @param url An URL to request Token History API.
+     * @param chainId The Klaytn network chain id.
+     * @param accessKeyId The access key provided by KAS console.
+     * @param secretAccessKey The secret key provided by KAS console.
+     */
     public KAS initTokenHistoryAPI(String url, String chainId, String accessKeyId, String secretAccessKey) {
         ApiClient apiClient = new ApiClient();
         apiClient.setBasePath(url);
@@ -60,26 +98,50 @@ public class KAS {
         return this;
     }
 
+    /**
+     * Getter function for anchor.
+     * @return Anchor API
+     */
     public AnchorAPI getAnchor() {
         return anchor;
     }
 
+    /**
+     * Setter function for anchor.
+     * @param anchor The Anchor API instance.
+     */
     public void setAnchor(AnchorAPI anchor) {
         this.anchor = anchor;
     }
 
+    /**
+     * Getter function for Token History
+     * @return TokenHistoryAPI.
+     */
     public TokenHistoryAPI getTokenHistory() {
         return tokenHistory;
     }
 
+    /**
+     * Setter function for Token History.
+     * @param tokenHistory The Token History API Instance
+     */
     public void setTokenHistory(TokenHistoryAPI tokenHistory) {
         this.tokenHistory = tokenHistory;
     }
 
+    /**
+     * Getter function for wallet
+     * @return WalletAPI
+     */
     public WalletAPI getWallet() {
         return wallet;
     }
 
+    /**
+     * Setter function for Wallet
+     * @param wallet The WalletAPI instance.
+     */
     public void setWallet(WalletAPI wallet) {
         this.wallet = wallet;
     }
