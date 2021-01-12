fc8ee81a8fbf614dd90492299ad9426a779c8622
kale <kale.kim@groundx.xyz>
2021-01-12 17:46:06 +0900

    Update src/main/java/xyz/groundx/caver_ext_kas/kas/wallet/WalletQueryOptions.java
    Co-authored-by: Jasmine <32922423+jimni1222@users.noreply.github.com>

--- src/main/java/xyz/groundx/caver_ext_kas/kas/wallet/WalletQueryOptions.java
+++ src/main/java/xyz/groundx/caver_ext_kas/kas/wallet/WalletQueryOptions.java
@@ -39,8 +39,8 @@ public class WalletQueryOptions {
         }
 
         /**
-         * Check if there is an enum mapped to the given type string.
-         * @param status The type string to find enum defined in LABEL_STATUS
+         * Check if there is an enum mapped to the given status string.
+         * @param status The status string to find enum defined in LABEL_STATUS
          * @return boolean
          */
         public static boolean isExist(String status) {
