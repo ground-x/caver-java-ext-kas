5dcfa63b74ae9a99d0e02288693e3b416488a283
kale.kim <kale.kim@groundx.xyz>
2021-01-18 11:15:55 +0900

    add requestId field

--- src/main/java/xyz/groundx/caver_ext_kas/exception/ExceptionDetail.java
+++ src/main/java/xyz/groundx/caver_ext_kas/exception/ExceptionDetail.java
@@ -3,6 +3,7 @@ package xyz.groundx.caver_ext_kas.exception;
 public class ExceptionDetail {
     int code;
     String message = "";
+    String requestId = "";
 
     public ExceptionDetail() {
     }
@@ -13,5 +14,9 @@ public class ExceptionDetail {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getRequestId() {
+        return requestId;
     }
 }
