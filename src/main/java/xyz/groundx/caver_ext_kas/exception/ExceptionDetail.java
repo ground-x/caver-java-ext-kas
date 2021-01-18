62399dde058580329898e3bceaeb5ec38b372e68
kale.kim <kale.kim@groundx.xyz>
2021-01-18 11:16:33 +0900

    set to ignore unknown field

--- src/main/java/xyz/groundx/caver_ext_kas/exception/ExceptionDetail.java
+++ src/main/java/xyz/groundx/caver_ext_kas/exception/ExceptionDetail.java
@@ -1,5 +1,8 @@
 package xyz.groundx.caver_ext_kas.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExceptionDetail {
     int code;
     String message = "";
