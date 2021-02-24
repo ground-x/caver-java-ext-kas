ab7721109e536b31e1f84826047bd419d98f2332
kale <kale.kim@groundx.xyz>
2021-02-24 17:28:34 +0900

    Update src/test/java/xyz/groundx/caver_ext_kas/kas/tokenhistory/TokenHistoryTestData.java
    Co-authored-by: Jasmine <32922423+jimni1222@users.noreply.github.com>

--- src/test/java/xyz/groundx/caver_ext_kas/kas/tokenhistory/TokenHistoryTestData.java
+++ src/test/java/xyz/groundx/caver_ext_kas/kas/tokenhistory/TokenHistoryTestData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The caver-java-ext-kas Authors
+ * Copyright 2021 The caver-java-ext-kas Authors
  *
  * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
