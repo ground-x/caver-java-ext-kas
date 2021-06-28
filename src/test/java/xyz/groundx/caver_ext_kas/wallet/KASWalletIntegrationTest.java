68d0a8fe442e1ae09daba978e517e7f786126867
kale.kim <kale.kim@groundx.xyz>
2021-06-28 13:54:16 +0900

    apply a caver-java 1.6.3-rc.1

--- src/test/java/xyz/groundx/caver_ext_kas/wallet/KASWalletIntegrationTest.java
+++ src/test/java/xyz/groundx/caver_ext_kas/wallet/KASWalletIntegrationTest.java
@@ -17,6 +17,9 @@
 package xyz.groundx.caver_ext_kas.wallet;
 
 import com.klaytn.caver.Caver;
+import com.klaytn.caver.abi.datatypes.Type;
+import com.klaytn.caver.abi.datatypes.Utf8String;
+import com.klaytn.caver.abi.datatypes.generated.Uint256;
 import com.klaytn.caver.contract.Contract;
 import com.klaytn.caver.contract.ContractDeployParams;
 import com.klaytn.caver.contract.SendOptions;
@@ -30,9 +33,6 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.Utf8String;
-import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.protocol.exceptions.TransactionException;
 import org.web3j.utils.Numeric;
 import xyz.groundx.caver_ext_kas.CaverExtKAS;
